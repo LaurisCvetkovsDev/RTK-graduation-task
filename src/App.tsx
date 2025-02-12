f2135feb0bc29493260b85fa0ee20f563447eed1
Lauris <Lauris2232@gmail.com>
2025-02-12 14:24:44 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>hello world</p>
+      <img
+        src="https://media.tenor.com/47WAzOr8LT0AAAAj/spinning-pomodoro.gif"
+        className="center"
+      ></img>
+    </>
+  );
+}
+
+export default App;
